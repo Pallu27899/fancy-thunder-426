af3d99ed94c1a18d52b87e5f07323cdad94f6889
Pallavi Bobale <101388809+Pallu27899@users.noreply.github.com>
2022-09-29 21:27:04 +0530

    Add files via upload

---
+++ Automated Student Registration System/src/Exceptions/StudentException.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class StudentException extends Exception{
+
+	
+	public StudentException() {
+		// TODO Auto-generated constructor stub
+	}
+	public StudentException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+}
